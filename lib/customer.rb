24ecd0b638598ba28fc5a9e7b3e5849bbe1d1748
github username <github email address>
2020-02-06 21:24:25 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,31 @@
 class Customer
+    attr_reader :name, :age
+    @@all = []
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select do |meals|
+        meals.customer == self
+    end
+  end
   
+  def waiters
+    self.meals.map do |waiters|
+        waiters.waiter
+    end
+  end
+
+  def self.all 
+    @@all
+  end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,30 @@
 class Waiter
+    attr_reader :name, :years_of_experience
+    @@all = []
+    def initialize(name, years_of_experience)
+        @name = name
+        @years_of_experience = years_of_experience
+        @@all << self
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        self.meals.max do |tip_1, tip_2|
+            tip_1.tip <=> tip_2.tip
+        end.customer
+    end
+
+    def self.all 
+        @@all
+    end 
 
 end
